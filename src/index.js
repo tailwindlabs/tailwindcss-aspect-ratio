da58e8c32a3d887b7bc73afde0a20933d968ee16
Adam Wathan <adam.wathan@gmail.com>
2020-11-06 14:19:55 -0500

    Init

---
+++ src/index.js
@@ -0,0 +1,85 @@
+const plugin = require('tailwindcss/plugin')
+
+const aspectRatio = plugin(
+  function ({ addComponents, theme, variants, e }) {
+    const values = theme('aspectRatio')
+
+    const baseSelectors = Object.entries(values)
+      .map(([key, value]) => {
+        return `.${e(`aspect-w-${key}`)}`
+      })
+      .join(',\n')
+
+    const childSelectors = Object.entries(values)
+      .map(([key, value]) => {
+        return `.${e(`aspect-w-${key}`)} > *`
+      })
+      .join(',\n')
+
+    console.log(baseSelectors, childSelectors)
+
+    addComponents(
+      [
+        {
+          [baseSelectors]: {
+            position: 'relative',
+            paddingBottom: `calc(var(--aspect-h) / var(--aspect-w) * 100%)`,
+          },
+        },
+        {
+          [childSelectors]: {
+            position: 'absolute',
+            height: '100%',
+            width: '100%',
+            top: '0',
+            right: '0',
+            bottom: '0',
+            left: '0',
+          },
+        },
+        Object.entries(values).map(([key, value]) => {
+          return {
+            [`.${e(`aspect-w-${key}`)}`]: {
+              '--aspect-w': value,
+            },
+          }
+        }),
+        Object.entries(values).map(([key, value]) => {
+          return {
+            [`.${e(`aspect-h-${key}`)}`]: {
+              '--aspect-h': value,
+            },
+          }
+        }),
+      ],
+      variants('aspectRatio')
+    )
+  },
+  {
+    theme: {
+      aspectRatio: {
+        1: '1',
+        2: '2',
+        3: '3',
+        4: '4',
+        5: '5',
+        6: '6',
+        7: '7',
+        8: '8',
+        9: '9',
+        10: '10',
+        11: '11',
+        12: '12',
+        13: '13',
+        14: '14',
+        15: '15',
+        16: '16',
+      },
+    },
+    variants: {
+      aspectRatio: ['responsive'],
+    },
+  }
+)
+
+module.exports = aspectRatio
